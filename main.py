eec78c3e2c67c8b3ddda9ffb5606ed219d94d528
vikasisking <vikashgup9793@gmail.com>
2025-08-19 16:17:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ REQUIRED_CHANNELS = [
     #"@+UyoEvMS5XAxkNTc0",
     #"@+2P-OUmWo1hc0NmNh",
    # "@fasttech3",
-    "@h2icoder"
+    "@h2icoder",
+    "@tips_tricks_shop"
 ]
 #channel2 = [ "@+F4Md7IotaqcxN2I9" ]
 # Store user IDs who have started the bot
@@ -77,6 +78,7 @@ def get_join_channel_message():
        # keyboard.append([InlineKeyboardButton(f"Join {channel2}", url=url)])
     keyboard.append([InlineKeyboardButton("✅ Check Membership", callback_data="check_membership")])
     return (
+        "Join Backup Channel First:- https://t.me/+F4Md7IotaqcxN2I9"\n\n
         "⚠️ <b>Please join all required channels to use this bot!</b>\n\n"
         "Click the buttons below to join the channels, then press 'Check Membership'.\n"
         "━━━━━━━━━━━━━━━━━━━━━━\n"
